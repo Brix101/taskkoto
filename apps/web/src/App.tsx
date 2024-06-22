d237282a04bde196d05608ea26fc4f22488f07f8
Brix <brixterporras@gmail.com>
2024-06-22 17:46:14 +0800

    feat: added initial client query

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,12 @@
 import "@/assets/globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { graphql } from "gql.tada";
+import { request } from "graphql-request";
 
 const queryClient = new QueryClient();
 
@@ -14,11 +20,42 @@ function App() {
   );
 }
 
+const query = graphql(`
+  query Tasks {
+    tasks {
+      id
+      title
+      assignee {
+        id
+        email
+        fullName
+      }
+      createdBy {
+        id
+        email
+        fullName
+      }
+    }
+  }
+`);
+
 function Sample() {
+  const { data } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => request("http://localhost:5000/graphql", query),
+  });
+
+  console.log(data);
   return (
     <>
-      {" "}
       <h1 className="bg-red-300 p-10">Hello world</h1>
+      <ul>
+        {data?.tasks.map((task) => (
+          <li key={task.id}>
+            {task.title}--{task.id}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
