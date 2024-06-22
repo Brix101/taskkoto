1d183d3db48462c998a2faecade06fa3593b54dd
Brix <brixterporras@gmail.com>
2024-06-22 17:30:59 +0800

    feat: added tanstack query

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,23 @@
 import "@/assets/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+      <Sample />
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+function Sample() {
+  return (
     <>
+      {" "}
       <h1 className="bg-red-300 p-10">Hello world</h1>
     </>
   );
